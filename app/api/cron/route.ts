2a50f0bbee9aa49aeb1564c4e97496e2aa025508
Abhishek dilip Mestri <borde7573@gmail.com>
2024-12-21 20:20:06 +0530

    fix build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,7 +6,7 @@ import { scrapeAmazonProduct, scrapeFlipkartProduct, scrapeMeeshoProduct } from 
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import { User } from '@/types';
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 60; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
