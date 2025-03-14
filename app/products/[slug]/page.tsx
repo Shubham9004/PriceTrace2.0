51cbe049c213b1d1823dca0578b3d81220180860
Shubham9004 <borde7573@gmail.com>
2025-03-14 19:53:41 +0530

    Some Changes

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -260,13 +260,15 @@ const ProductDetails = async ({ params: { slug } }: Props) => {
         </div>
 
         <Link 
-  href="/" 
+  href={product.url} 
+  target="_blank"
   className="btn w-fit mx-auto flex items-center justify-center gap-3 min-w-[200px]" 
   aria-label="Buy Now"
 >
   <Image src="/assets/icons/bag.svg" alt="check" width={22} height={22} />
   <span className="text-base text-white">Buy Now</span>
 </Link>
+
       </div>
 
       {similarProducts && similarProducts.length > 0 && (
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,31 +1,27 @@
 import { Product } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React from 'react'
 
 interface Props {
   product: Product;
 }
-
+ 
 const ProductCard = ({ product }: Props) => {
   return (
-    <div className="product-card">
-      <Link href={`/products/${product.slug}`} className="block">
-        <div className="product-card_img-container">
-          <Image 
-            src={product.image}
-            alt={product.title}
-            width={200}
-            height={200}
-            className="product-card_img"
-          />
-        </div>
-      </Link>
+    <Link href={`/products/${product.slug}`} className="product-card">
+      <div className="product-card_img-container">
+        <Image 
+          src={product.image}
+          alt={product.title}
+          width={200}
+          height={200}
+          className="product-card_img"
+        />
+      </div>
 
       <div className="flex flex-col gap-3">
-        <Link href={`/products/${product.slug}`} className="block">
-          <h3 className="product-title">{product.title}</h3>
-        </Link>
+        <h3 className="product-title">{product.title}</h3>
 
         <div className="flex justify-between">
           <p className="text-black opacity-50 text-lg capitalize">
@@ -33,13 +29,13 @@ const ProductCard = ({ product }: Props) => {
           </p>
 
           <p className="text-black text-lg font-semibold">
-            <span>{product.currency}</span>
-            <span>{product.currentPrice}</span>
+            <span>{product?.currency}</span>
+            <span>{product?.currentPrice}</span>
           </p>
         </div>
       </div>
-    </div>
-  );
-};
+    </Link>
+  )
+}
 
-export default ProductCard;
+export default ProductCard
